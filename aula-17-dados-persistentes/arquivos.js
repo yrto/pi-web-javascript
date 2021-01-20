0d81d6502827578d6d70f7c30d400c6e84ccc7b5
Ayrton Mugnaini <ayrton.mugnaini@gmail.com>
2021-01-20 11:19:00 -0300

    Atualização no arquivo de dados .JSON

--- aula-17-dados-persistentes/arquivos.js
+++ aula-17-dados-persistentes/arquivos.js
@@ -4,6 +4,8 @@ const fs = require("fs")
 
 let dados = JSON.parse(fs.readFileSync("dados.json").toString())
 
+dados.users.push({ nome: "Bruno", idade: 28 })
+
 fs.writeFileSync("dados.json", JSON.stringify(dados))
 
 console.log(dados)
